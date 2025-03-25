446d1b48daef1d6f88e88c630fd01bb8d37de55f
Ickise <enzo.dufour05@gmail.com>
2025-03-26 00:30:33 +0100

    EnergyManager

---
+++ Assets/Script/Managers/EnergyManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnergyManager : MonoBehaviour
+{
+    public static EnergyManager instance;
+
+    private int maxEnergy = 100;
+    private int currentEnergy;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        currentEnergy = maxEnergy;
+    }
+
+    public void ChangeEnergyValue(int amount, bool isIncreasing)
+    {
+        currentEnergy = isIncreasing ? currentEnergy + amount : currentEnergy - amount;
+        currentEnergy = Mathf.Clamp(currentEnergy, 0, maxEnergy);
+    }
+}
