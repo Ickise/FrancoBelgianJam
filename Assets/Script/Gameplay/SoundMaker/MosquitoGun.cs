80f2911310785d47fb9897fa887344c23df8033f
Enzo Dufour <enzo.dufour05@gmail.com>
2025-03-26 20:02:59 +0100

    Update

--- Assets/Script/Gameplay/SoundMaker/MosquitoGun.cs
+++ Assets/Script/Gameplay/SoundMaker/MosquitoGun.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 
-public class SoundMaker : ToolBase
+public class MosquitoGun : ToolBase
 {
     [SerializeField, Header("References")] private GameObject soundObjectPrefab;
     [SerializeField] private GameObject powerfulSoundPrefab;
-    [SerializeField, Header("Settings")] private float holdThreshold = 2f;
-    [SerializeField] private float timeToDestroy = 5f;
+    [SerializeField, Header("Settings")] private float holdThreshold = 3f;
+    [SerializeField] private float littleDisturbAreaRange = 60f;
+    [SerializeField] private float littleDisturbAreDistance = 3f;
+    [SerializeField] private float bigDisturbAreaRange = 90f;
+    [SerializeField] private float bigDisturbAreDistance = 5f;
+    [SerializeField] private float bigDisturbAreaTimeLife = 2f;
+    [SerializeField] private float recoverTime = .5f;
 
     private float holdTime = 0f;
 
@@ -33,7 +38,7 @@ public class SoundMaker : ToolBase
 
             GameObject spawnedSound = Instantiate(soundToSpawn, transform.position, Quaternion.identity);
 
-            Destroy(spawnedSound, timeToDestroy);
+            Destroy(spawnedSound, bigDisturbAreaTimeLife);
 
             holdTime = 0f;
         }
