33c5dbe95cc8bc003c90bc621db6a434dfbfedb8
Veltaryom <calificeantoine724@gmail.com>
2025-03-26 11:56:41 +0100

    finish base of peace state of cow, change set pos of change level

--- Assets/Script/Cow.cs
+++ Assets/Script/Cow.cs
@@ -1,4 +1,4 @@
-
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,9 +9,10 @@ public class Cow : MonoBehaviour
     private float _currentTimeStatic;
     private int _currentRandTimeStatic;
     private Vector3 _peaceDirection;
-    [SerializeField] private int m_minDistance = 1;
-    [SerializeField] private int m_maxDistance = 3;
-    private int _currentRandDistance;
+    [SerializeField] private int m_minDistance = -1;
+    [SerializeField] private int m_maxDistance = 1;
+    private int _currentRandDistancex;
+    private int _currentRandDistancez;
     private Vector3 _randNextPosPeace;
     [SerializeField] private float m_speedPeace = 3f;
     [SerializeField] private int m_afraidDistance = 3;
@@ -22,9 +23,17 @@ public class Cow : MonoBehaviour
     [SerializeField] private int m_maxTimeRecoverFromFart = 9;
     [SerializeField] private int m_timeFartCreation = 2;
 
+    [SerializeField] private NavMeshAgent _navmesh;
+    [SerializeField] private CharacterController _charactercontroller;
+    [SerializeField] private LevelManager _levelManager;
+    private GameObject _currentWorld;
+    [SerializeField] private int _EdgeOfWorldToCenter;
+    [SerializeField]private float _roundDeplacement;
+
     private bool _peace = false;
     private bool _afraid = false;
     private bool _fart = false;
+    private bool _deplacementPeace = false;
 
     [SerializeField] private CowState _cowState;
     public enum CowState
@@ -35,38 +44,78 @@ public class Cow : MonoBehaviour
     }
     
     
-    void Start()
+    void Start()  // initial initialize of this value for a good start
     {
         _cowState = CowState.Peace;
         SwitchCowState(_cowState);
         _currentRandTimeStatic = 0;
         _currentTimeStatic = 0f;
-        _currentRandDistance = 0;
+        _currentRandDistancex = 0;
+        _currentRandDistancez = 0;
         _randNextPosPeace = new Vector3(0,0,0);
+        RandomTimeStatic();
+        RandomPeaceDistance();
     }
 
     
     void Update()
     {
-        if (_peace == true & _afraid == false & _fart == false)
+        if (_peace == true && _afraid == false && _fart == false)  // peace state     manque the other cow detection
         {
-            _currentTimeStatic = Mathf.MoveTowards(_currentTimeStatic, _currentRandTimeStatic, 1 * Time.deltaTime);
+            //print(" in peace State");
+            if (_currentTimeStatic != _currentRandTimeStatic)    
+            {
+                _currentTimeStatic = Mathf.MoveTowards(_currentTimeStatic, _currentRandTimeStatic, 1 * Time.deltaTime);
+                //print(_currentTimeStatic);
+            }
             if(_currentTimeStatic == _currentRandTimeStatic)
             {
-                RandomPeaceDistance();
-                _randNextPosPeace = new Vector3(transform.position.x + _currentRandDistance, 0, transform.position.z + _currentRandDistance);
-                _peaceDirection = _randNextPosPeace - transform.position;
+                print("deplacement");
+                _currentWorld = _levelManager.m_intermediateLevelSwitch;            // get the current world
+                if (_randNextPosPeace.x <= _currentWorld.transform.position.x +  _EdgeOfWorldToCenter ||
+                    _randNextPosPeace.x >= _currentWorld.transform.position.x -  _EdgeOfWorldToCenter && 
+                    _randNextPosPeace.z <= _currentWorld.transform.position.z +  _EdgeOfWorldToCenter ||
+                    _randNextPosPeace.z >= _currentWorld.transform.position.z -  _EdgeOfWorldToCenter && 
+                    _deplacementPeace == false)  // verify new random pos for cow by verify the bound of the current world 
+                {
+
+                    //_peaceDirection = _randNextPosPeace - transform.position;    // direction calcul
+                    //_navmesh.Move(_randNextPosPeace);                //
+                    //_charactercontroller.SimpleMove(m_speedPeace);  // was trying something for move but not succed
+                    _deplacementPeace = true;
+                }
+                else
+                {
+                    //RandomPeaceDistance();     // see if it worse to do that
+                }
+
+                if (_deplacementPeace == true)     // deplacement here
+                {
+                    //transform.position = Vector3.Lerp(transform.position, _randNextPosPeace, m_speedPeace * Time.deltaTime);
+                    transform.position = Vector3.LerpUnclamped(transform.position, _randNextPosPeace, m_speedPeace * Time.deltaTime); //made as alternative of incremental
+                    if (transform.position.x <= _randNextPosPeace.x + _roundDeplacement && 
+                        transform.position.z <= _randNextPosPeace.z + _roundDeplacement ||
+                        transform.position.x >= _randNextPosPeace.x - _roundDeplacement && 
+                        transform.position.z >= _randNextPosPeace.z - _roundDeplacement) // initialize new timer when x and z are equal (y change i don't know why)
+                    {
+                        _deplacementPeace = false;
+                        RandomTimeStatic();
+                        RandomPeaceDistance();
+                        _currentTimeStatic = 0;       //end of cycle
+                    }
+                }
+                
             }
         }
 
-        if (_peace == false & _afraid == true & _fart == false)
+        if (_peace == false & _afraid == true & _fart == false) // afraid state
         {
-            
+            print(" in afraid State");
         }
 
-        if (_peace == false & _afraid == false & _fart == true)
+        if (_peace == false & _afraid == false & _fart == true) // fart state
         {
-            
+            print(" in fart State");
         }
     }
 
@@ -77,7 +126,7 @@ public class Cow : MonoBehaviour
             case CowState.Peace:
             RandomTimeStatic();
             _currentTimeStatic = 0;
-            _currentRandDistance = 0;
+            _currentRandDistancex = 0;
             _peace = true;
             _afraid = false;
             _fart = false;
@@ -98,12 +147,16 @@ public class Cow : MonoBehaviour
         }
     }
 
-    private void RandomTimeStatic()
+    private void RandomTimeStatic() // get a random time for cow stay static
     {
         _currentRandTimeStatic = Random.Range(m_minTimeStatic, m_maxTimeStatic);
+        print("new time static = " + _currentRandTimeStatic);
     }
-    private void RandomPeaceDistance()
+    private void RandomPeaceDistance() // get a random pos x z to get a random pos
     {
-        _currentRandDistance = Random.Range(m_minDistance, m_maxDistance);
+        _currentRandDistancex = Random.Range(m_minDistance, m_maxDistance); 
+        _currentRandDistancez = Random.Range(m_minDistance, m_maxDistance); 
+        _randNextPosPeace = new Vector3(transform.position.x + _currentRandDistancex, 0, transform.position.z + _currentRandDistancez);    // compose the destination with the random offset on x z
+        print(_randNextPosPeace);
     }
 }
--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -1,19 +1,19 @@
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
     [SerializeField] private int m_levelSwitch = 1;
     [SerializeField] private GameObject m_character;
     [SerializeField] private List<GameObject> m_positionLevel;
-    private GameObject m_intermediateLevelSwitch;
+    public GameObject m_intermediateLevelSwitch;
+    [SerializeField] private GameObject _camera;
     
     void Start()
     {
         //m_levelSwitch = 1;
-        SwitchLevel(m_levelSwitch);
+        SwitchLevel(m_levelSwitch);  // initial set up
     }
 
     public void Update() //debug only
@@ -26,25 +26,29 @@ public class NewMonoBehaviourScript : MonoBehaviour
         switch (m_levelSwitch)
         {
             case 0:
-            m_intermediateLevelSwitch = m_positionLevel.ElementAt(m_levelSwitch);
-            m_character.transform.position = m_intermediateLevelSwitch.transform.position;
+            m_intermediateLevelSwitch = m_positionLevel.ElementAt(m_levelSwitch);               //set the new world with index
+            m_character.transform.position = m_intermediateLevelSwitch.transform.position;       // set position of character to this new world   
+            _camera.transform.position = m_intermediateLevelSwitch.transform.position;            // same with camera    warning change the parent pos, not the camera component itself
                 break;
 
 
             case 1:
             m_intermediateLevelSwitch = m_positionLevel.ElementAt(m_levelSwitch);
             m_character.transform.position = m_intermediateLevelSwitch.transform.position;
+            _camera.transform.position = m_intermediateLevelSwitch.transform.position;
                 break;
 
 
             case 2:
             m_intermediateLevelSwitch = m_positionLevel.ElementAt(m_levelSwitch);
             m_character.transform.position = m_intermediateLevelSwitch.transform.position;
+            _camera.transform.position = m_intermediateLevelSwitch.transform.position;
                 break;
 
             case 3:
             m_intermediateLevelSwitch = m_positionLevel.ElementAt(m_levelSwitch);
             m_character.transform.position = m_intermediateLevelSwitch.transform.position;
+            _camera.transform.position = m_intermediateLevelSwitch.transform.position;
                 break;
 
             
