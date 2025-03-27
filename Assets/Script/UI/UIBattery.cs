aeda2673dda8c2617470f01cb7fe2478c2cf272f
RicoParmentier <ricoparmentier@gmail.com>
2025-03-27 22:41:53 +0100

    UI stuff

--- Assets/Script/UI/UIBattery.cs
+++ Assets/Script/UI/UIBattery.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class UIBattery : MonoBehaviour
 {
     [SerializeField, Header("References")] private TextMeshProUGUI batteryText;
 
     [SerializeField] private BatteryManager _batteryManager;
+    [SerializeField] private GameObject[] _batteryGO;
 
     private void Start()
     {
@@ -16,5 +18,65 @@ public class UIBattery : MonoBehaviour
     public void UpdateEnergyUI()
     {
         batteryText.text = $"Energy: {_batteryManager.GetCurrentBattery()} / {_batteryManager.GetMaxBattery()}";
+        switch (_batteryManager.GetCurrentBattery())
+        {
+            case 0:
+                _batteryGO[0].SetActive(false);
+                _batteryGO[1].SetActive(false);
+                _batteryGO[2].SetActive(false);
+                _batteryGO[3].SetActive(false);
+                _batteryGO[4].SetActive(false);
+                _batteryGO[5].SetActive(false);
+                break;
+            case <=17:
+                _batteryGO[0].SetActive(true);
+                _batteryGO[1].SetActive(false);
+                _batteryGO[2].SetActive(false);
+                _batteryGO[3].SetActive(false);
+                _batteryGO[4].SetActive(false);
+                _batteryGO[5].SetActive(false);
+                break;
+            case <=34:
+                _batteryGO[0].SetActive(true);
+                _batteryGO[1].SetActive(true);
+                _batteryGO[2].SetActive(false);
+                _batteryGO[3].SetActive(false);
+                _batteryGO[4].SetActive(false);
+                _batteryGO[5].SetActive(false);
+                break;
+            case <=51:
+                _batteryGO[0].SetActive(true);
+                _batteryGO[1].SetActive(true);
+                _batteryGO[2].SetActive(true);
+                _batteryGO[3].SetActive(false);
+                _batteryGO[4].SetActive(false);
+                _batteryGO[5].SetActive(false);
+                break;
+            case <=68:
+                _batteryGO[0].SetActive(true);
+                _batteryGO[1].SetActive(true);
+                _batteryGO[2].SetActive(true);
+                _batteryGO[3].SetActive(true);
+                _batteryGO[4].SetActive(false);
+                _batteryGO[5].SetActive(false);
+                break;
+            case <=85:
+                _batteryGO[0].SetActive(true);
+                _batteryGO[1].SetActive(true);
+                _batteryGO[2].SetActive(true);
+                _batteryGO[3].SetActive(true);
+                _batteryGO[4].SetActive(true);
+                _batteryGO[5].SetActive(false);
+                break;
+            case <=100:
+                _batteryGO[0].SetActive(true);
+                _batteryGO[1].SetActive(true);
+                _batteryGO[2].SetActive(true);
+                _batteryGO[3].SetActive(true);
+                _batteryGO[4].SetActive(true);
+                _batteryGO[5].SetActive(true);
+                break;
+        }
+
     }
 }
