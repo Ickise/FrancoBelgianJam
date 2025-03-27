6113c9d989e227ecca191c2448f2b4e063a30234
Enzo Dufour <enzo.dufour05@gmail.com>
2025-03-27 13:07:40 +0100

    Update CowController.cs

--- Assets/Script/Gameplay/Cow/CowController.cs
+++ Assets/Script/Gameplay/Cow/CowController.cs
@@ -5,15 +5,12 @@ public class CowController : MonoBehaviour
 {
     private ICowState currentState;
 
-    [SerializeField] private int minTimeStatic = 3;
-    [SerializeField] private int maxTimeStatic = 5;
     [SerializeField] private int minDistance = 1;
     [SerializeField] private int maxDistance = 3;
     [SerializeField] private float speedPeace = 3f;
     [SerializeField] private int afraidDistance = 3;
     [SerializeField] private float speedAfraid = 5f;
     [SerializeField] private int fartDistance = 9;
-    [SerializeField] private float speedFart = 9f;
     [SerializeField] private int minTimeRecoverFromFart = 5;
     [SerializeField] private int maxTimeRecoverFromFart = 9;
     [SerializeField] private int timeFartCreation = 2;
@@ -52,18 +49,14 @@ public class CowController : MonoBehaviour
         }
     }
 
-    public int GetMinTimeStatic() => minTimeStatic;
-    public int GetMaxTimeStatic() => maxTimeStatic;
     public int GetMinDistance() => minDistance;
     public int GetMaxDistance() => maxDistance;
     public float GetSpeedPeace() => speedPeace;
     public int GetAfraidDistance() => afraidDistance;
     public float GetSpeedAfraid() => speedAfraid;
     public int GetFartDistance() => fartDistance;
-    public float GetSpeedFart() => speedFart;
     public int GetMinTimeRecoverFromFart() => minTimeRecoverFromFart;
     public int GetMaxTimeRecoverFromFart() => maxTimeRecoverFromFart;
-    public int GetTimeFartCreation() => timeFartCreation;
     
     public bool HasBeenScared() => hasBeenScared;
     public bool HasBeenFarted() => hasBeenFarted;
@@ -72,9 +65,6 @@ public class CowController : MonoBehaviour
     
     public NavMeshAgent NavMeshAgent => navMeshAgent;
 
-    public void SetMoveDirection(Vector3 direction) => moveDirection = direction;
-    public Vector3 GetMoveDirection() => moveDirection;
-    public bool IsFarting() => isFarting;
     public void SetFarting(bool value) => isFarting = value;
     
     public void SpawnFart()
