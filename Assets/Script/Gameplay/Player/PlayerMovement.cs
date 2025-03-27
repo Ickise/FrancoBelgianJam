4311d68c7b680ef0b7148b49d945ae5f0676d9ed
Enzo Dufour <enzo.dufour05@gmail.com>
2025-03-27 11:24:22 +0100

    Update PlayerMovement.cs

--- Assets/Script/Gameplay/Player/PlayerMovement.cs
+++ Assets/Script/Gameplay/Player/PlayerMovement.cs
@@ -35,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void GetInputValue(Vector2 direction)
     {
-        movement = new Vector3(direction.x, 0, direction.y);
+        movement = new Vector3(direction.x, 0, direction.y).normalized;
     }
 
     private void Start()
@@ -64,10 +64,12 @@ public class PlayerMovement : MonoBehaviour
     private void MovePlayer()
     {
         var currentSpeed = batteryManager.BatteryOvercharging() ? overchargeSpeed : speed;
+        Vector3 velocity = playerRigidbody.linearVelocity;
 
-        playerRigidbody.linearVelocity = gasManager.IsGasStockOverFilled()
-            ? movement * overfillSpeed
-            : movement * currentSpeed;
+        velocity.x = movement.x * (gasManager.IsGasStockOverFilled() ? overfillSpeed : currentSpeed);
+        velocity.z = movement.z * (gasManager.IsGasStockOverFilled() ? overfillSpeed : currentSpeed);
+
+        playerRigidbody.linearVelocity = velocity;
 
         playerRotation.RotatePlayer();
     }
