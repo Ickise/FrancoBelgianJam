46de7588505679d73c38d723b8ed2a48e9a98545
MSI Félix <felix.sun210302@gmail.com>
2025-03-27 21:14:50 +0100

    fix: time for upgrade

--- Assets/Script/Managers/UpgradeManager.cs
+++ Assets/Script/Managers/UpgradeManager.cs
@@ -66,8 +66,14 @@ public class UpgradeManager : MonoBehaviour
         if (state)
         {
             ChooseRangeRandomUpgrade();
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
         }
         uiMenuUpgrade.SetActive(state);
+        
     }
 
     private void ChooseRangeRandomUpgrade()
