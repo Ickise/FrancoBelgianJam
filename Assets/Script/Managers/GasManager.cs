58b286bd17a08d1281ec7a0663ef96b64b23bec8
Ickise <enzo.dufour05@gmail.com>
2025-03-27 23:35:24 +0100

    Update GasManager.cs

--- Assets/Script/Managers/GasManager.cs
+++ Assets/Script/Managers/GasManager.cs
@@ -14,7 +14,7 @@ public class GasManager : MonoBehaviour
     
     private GameManager gameManager;
 
-    private float currentGasStock = 1500;
+    private float currentGasStock = 0;
 
     private void Awake()
     {
